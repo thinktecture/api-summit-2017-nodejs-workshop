b56596f9821446f1e7feb871e271a980b73ec07c
Manuel Rauber <manuel.rauber@thinktecture.com>
2017-06-20 15:24:55 +0200

    Azure Deploy App

--- deploy.app.sh
+++ deploy.app.sh
@@ -112,7 +112,7 @@ selectNodeVersion
 # 3. Install npm packages
 if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   cd "$DEPLOYMENT_TARGET"
-  eval $NPM_CMD install --production
+  eval $NPM_CMD install
   exitWithMessageOnError "npm failed"
   eval $NPM_CMD run release
   cd - > /dev/null
